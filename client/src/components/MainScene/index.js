c23e250deba8c09b25026b7bc1091c3d06e54809
Freddy Cheungh <fgcc1111@gmail.com>
2017-12-02 10:09:54 -0500

    Made AR Scene work

--- client/src/components/MainScene/index.js
+++ client/src/components/MainScene/index.js
@@ -2,13 +2,11 @@ import React, { Component } from "react";
 
 export default class MainScene extends Component {
     render() {
-        return (
-            <a-scene embedded arjs>
-                <a-marker preset="hiro">
-                    <a-box position='0 0.5 0' material='color: black;'></a-box>
-                </a-marker>
-                <a-entity camera></a-entity>
-            </a-scene>
-        );
+        return [
+            <a-marker preset="hiro">
+                <a-box position='0 0.5 0' material='color: black;'></a-box>
+            </a-marker>,
+            <a-entity camera></a-entity>
+        ]
     }
 }
