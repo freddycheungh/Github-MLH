ecdcf5e2603cb046c4f37d3317dfb77cf215ceab
ericschv <ericschv@gmail.com>
2017-12-02 10:54:40 -0500

    added look controls to move 3D object

--- client/src/components/MainScene/index.js
+++ client/src/components/MainScene/index.js
@@ -4,9 +4,9 @@ export default class MainScene extends Component {
     render() {
         return [
             <a-marker preset="hiro">
-                <a-box position='0 0.5 0' material='color: black;'></a-box>
+                <a-box position='0 0.5 0' material='color: black'></a-box>
             </a-marker>,
-            <a-entity camera></a-entity>
+            <a-entity camera look-controls></a-entity>
         ]
     }
 }
